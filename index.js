f18ff5e984a91a79030d5f653cf5e4495c7cfe8c
Gui Lin <guileen@gmail.com>
2014-02-21 13:05:09 +0800

    replace tab with space

--- index.js
+++ index.js
@@ -76,37 +76,37 @@ var exports = module.exports = function(routes) {
         }
     }
 
-	var beforeHandler, afterHandler;
-	var server = http.createServer(function(req, res) {
-		var d = domain.create();
-		d.on('error', function(err) {
-			server.emit('domainError', err, req, res);
-		})
-		d.add(req);
-		d.add(res);
-		d.run(function() {
-			var method = req.method.toLowerCase();
-			var urlinfo = parseUrl(req.url, true);
-			req.urlinfo = urlinfo;
-			beforeHandler && beforeHandler(req, res);
-			afterHandler && res.on('finish', function(){
-				afterHandler(req, res);
-			});
-			var key = method + ':' + urlinfo.pathname;
+    var beforeHandler, afterHandler;
+    var server = http.createServer(function(req, res) {
+        var d = domain.create();
+        d.on('error', function(err) {
+            server.emit('domainError', err, req, res);
+        })
+        d.add(req);
+        d.add(res);
+        d.run(function() {
+            var method = req.method.toLowerCase();
+            var urlinfo = parseUrl(req.url, true);
+            req.urlinfo = urlinfo;
+            beforeHandler && beforeHandler(req, res);
+            afterHandler && res.on('finish', function(){
+                afterHandler(req, res);
+            });
+            var key = method + ':' + urlinfo.pathname;
             var route = routes[key] || matchRegexRoutes(key, req) || routes['404'] || default404;
             route(req, res);
-		})
-	});
+        })
+    });
 
-	server.before = function(_beforeHandler) {
-		beforeHandler = _beforeHandler;
-		return server;
-	}
+    server.before = function(_beforeHandler) {
+        beforeHandler = _beforeHandler;
+        return server;
+    }
 
-	server.after = function (_afterHandler) {
-		afterHandler = _afterHandler;
-		return server;
-	}
+    server.after = function (_afterHandler) {
+        afterHandler = _afterHandler;
+        return server;
+    }
     ;[
         'acl'
       , 'aseline-control'
@@ -142,7 +142,7 @@ var exports = module.exports = function(routes) {
                 defineRoute(verb, path, fn);
             }
     });
-	return server;
+    return server;
 }
 // req.urlinfo  see url.parse
 // magic code
